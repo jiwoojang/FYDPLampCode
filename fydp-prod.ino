8a1e0d8e5b286aae3df21f04d2400df787957e9d
Jiwoo Jang <jiwooo.jang@gmail.com>
2020-02-29 17:54:52 -0500

    Update firmware with latest from recent work

--- fydp-prod.ino
+++ fydp-prod.ino
@@ -13,14 +13,9 @@ int rgbOnOff = A2;
 int coldOnOff = A1;
 int warmOnOff = A0;
 
-// These control the R and G values as digital I/O
-int redOnOff = D4;
-int greenOnOff = D3;
-
 // These control values as PWM
-int blueBrightness = D2; 
-int coldBrightness = D1;
-int warmBrightness = D0;
+int blueBrightness = D1;
+int otherBrightness = D0;
 
 //------------------------------------------//
 //               Custom Types
@@ -52,19 +47,16 @@ void setupPins()
     pinMode(brightness, AN_INPUT);
     
     // Digital input
-    pinMode(ambientFocusSwitch, INPUT);
+    pinMode(ambientFocusSwitch, INPUT_PULLUP);
 
     // Digital outputs
     pinMode(rgbOnOff, OUTPUT);
     pinMode(coldOnOff, OUTPUT);
     pinMode(warmOnOff, OUTPUT);
-    pinMode(redOnOff, OUTPUT);
-    pinMode(greenOnOff, OUTPUT);
     
     // PWM outputs
     pinMode(blueBrightness, OUTPUT);
-    pinMode(coldBrightness, OUTPUT);
-    pinMode(warmBrightness, OUTPUT);
+    pinMode(otherBrightness, OUTPUT);
     
     // FOR DEBUGGING PURPOSES
     pinMode(D7, OUTPUT);
@@ -97,19 +89,97 @@ void setupCloudFunctions()
 //------------------------------------------//
 //              Main init
 //------------------------------------------//
-void setup() 
+void setup()
 {
     // Set everything up
     setupPins();
     setupCloudFunctions();
     
+    digitalWrite(rgbOnOff, HIGH);
+    digitalWrite(coldOnOff, HIGH);
+    digitalWrite(warmOnOff, HIGH);
+    
     // Initialize all global values to their starting values
 }
+
+int blueVal = 0;
+
+int prevARead = 0;
+
+int prevDRead = HIGH;
+int lastDRead = LOW;
+
+unsigned long lastDebounceTime = 0;
+unsigned long debounceDelay = 50;
+
+int brightnessVal = 0;
+
+int state = 0;
 
 //------------------------------------------//
 //              Main loop
 //------------------------------------------//
 void loop() 
 {
+    int aRead = analogRead(brightness);
+    //Serial.printlnf("aRead: %d prevARead %d\n delta: %d", aRead, prevARead, abs(aRead - prevARead));
 
+    if (abs(aRead - prevARead) > 60)
+    {
+        prevARead = aRead;
+        float norm = aRead / 4095.0;
+        brightnessVal = (int)(255 * norm);
+        
+        analogWrite(otherBrightness, brightnessVal, 200);
+        analogWrite(blueBrightness, brightnessVal, 200);
+        
+        Serial.printlnf("Setting brigntness to %d\n", brightnessVal);
+    }
+    
+    int dRead = digitalRead(ambientFocusSwitch);
+    
+    if (dRead != lastDRead)
+    {
+        lastDebounceTime = millis();
+    }
+    
+    if ((millis() - lastDebounceTime) > debounceDelay) 
+    {
+        if (dRead != prevDRead) 
+        {
+            prevDRead = dRead;
+            
+            if (prevDRead == 0)
+            {
+                state++;
+                
+                state = state % 3;
+                Serial.printlnf("Switching state to %d\n", brightnessVal);
+            }
+        }
+    }
+    
+    lastDRead = dRead;
+    
+    if (state == 0)
+    {
+        digitalWrite(coldOnOff, HIGH);
+        digitalWrite(warmOnOff, HIGH);
+
+        digitalWrite(rgbOnOff, LOW);
+    }
+    else if(state == 1)
+    {
+        digitalWrite(warmOnOff, HIGH);
+        digitalWrite(rgbOnOff, HIGH);
+
+        digitalWrite(coldOnOff, LOW);
+    }
+    else if(state == 2)
+    {
+        digitalWrite(coldOnOff, HIGH);
+        digitalWrite(rgbOnOff, HIGH);
+
+        digitalWrite(warmOnOff, LOW);
+    }
 }
